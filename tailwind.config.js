671ce9898c72369ab98cdda14e08aff7f8ddb463
Fenet04 <133098255+Fenet04@users.noreply.github.com>
2024-01-05 21:08:55 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'cocoa':'#92400e',
+      'coffee':'#B19470',
+      'mocha':'#92400e',
+      'cream':'#EAC696',
+      'caramel':'#ca8a04',
+      'almond': '#F8FAE5', 
+      'Taupe': '#76453B',
+      'forest': '#43766C',
+      'delete': '#B71C1C',
+      'deletehover': '#FF8A80'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
